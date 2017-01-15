37578acf28dba6a41c39056e3c9cd60cf54dcadd
John Lim <penandlim@gmail.com>
2017-01-15 09:09:31 -0600

    minimum distance fixed

--- js/app.js
+++ js/app.js
@@ -191,8 +191,8 @@
                 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
                 var distance = radius_earth * c;
-                if (distance < 3) {
-                    distance = 3;
+                if (distance < 1) {
+                    distance = 1;
                 }
                 item.innerText = parseInt(distance) + "m";
 
@@ -213,11 +213,11 @@
                 if (typeof item.style.transform !== "undefined") {
                     item.style.transform = "rotateZ(" + (-1 * angle) + "deg)";
                     item.style.transformOrigin = "50% 200px";
-                    item.style.transform += "translate(0px, " + ((distance / -1.2) - (!isCloseToInteractWith * 50)) + "px)";
+                    item.style.transform += "translate(0px, " + ((distance / -0.8) - (!isCloseToInteractWith * 50)) + "px)";
                 } else if (typeof item.style.webkitTransform !== "undefined") {
                     item.style.webkitTransform = "rotateZ(-" + (-1 * angle) + "deg)";
                     item.style.webkitTransformOrigin = "50% 200px";
-                    item.style.webkitTransform += "translate(0px, " + ((distance / -1.2) - (!isCloseToInteractWith * 50))  + "px)";
+                    item.style.webkitTransform += "translate(0px, " + ((distance / -0.8) - (!isCloseToInteractWith * 50))  + "px)";
                 }
 
 
