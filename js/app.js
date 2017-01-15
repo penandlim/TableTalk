4197e7d2361a0e12375e120ab965bf1f70a65dab
John Lim <penandlim@gmail.com>
2017-01-14 20:49:59 -0600

    doesnt work plz halp

--- js/app.js
+++ js/app.js
@@ -155,7 +155,7 @@
                 var x = Math.cos(lat1)*Math.sin(lat2) -
                     Math.sin(lat1)*Math.cos(lat2)*Math.cos(lng2-lng1);
                 var brng = Math.atan2(y, x).toDegrees();
-                document.getElementById("_1").innerText = "(" + brng + ")";
+                document.getElementById("_" + (i+1)).innerText = "(" + brng + ")"; // ?? not hitting this
                 var angle = positionCurrent.hng - brng;
                 if (typeof item.style.transform !== "undefined") {
                     item.style.transform = "rotateZ(" + angle + "deg)";
