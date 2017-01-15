7e432eab68858a612da8be66d3cc5d8eeb0f6af2
John Lim <penandlim@gmail.com>
2017-01-15 03:20:18 -0600

    are the points too far?

--- js/app.js
+++ js/app.js
@@ -198,11 +198,11 @@
                 if (typeof item.style.transform !== "undefined") {
                     item.style.transform = "rotateZ(" + (-1 * angle) + "deg)";
                     item.style.transformOrigin = "50% 200px";
-                    item.style.transform += "translate(0px, " + (distance / -3) + "px)";
+                    item.style.transform += "translate(0px, " + (distance / -10) + "px)";
                 } else if (typeof item.style.webkitTransform !== "undefined") {
                     item.style.webkitTransform = "rotateZ(-" + (-1 * angle) + "deg)";
                     item.style.transformOrigin = "50% 200px";
-                    item.style.transform += "translate(0px, " + (distance / -3) + "px)";
+                    item.style.transform += "translate(0px, " + (distance / -10) + "px)";
                 }
             }
 
