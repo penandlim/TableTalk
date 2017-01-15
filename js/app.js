4c18507530b6ed6dae4e9c78d547c1e76282ac50
John Lim <penandlim@gmail.com>
2017-01-14 21:41:32 -0600

    does it work????? negative top padding

--- js/app.js
+++ js/app.js
@@ -158,11 +158,11 @@
                 var angle = positionCurrent.hng - brng;
                 if (typeof item.style.transform !== "undefined") {
                     item.style.transform = "rotateZ(" + angle + "deg)";
-                    item.style.transform = "transform-origin: 50% 400%";
+                    item.style.transformOrigin = "50% 400%";
                     item.style.paddingTop = (-70 * (i + 1)) + "px";
                 } else if (typeof item.style.webkitTransform !== "undefined") {
                     item.style.webkitTransform = "rotateZ(" + angle + "deg)";
-                    item.style.webkitTransform = "transform-origin: 50% 400%";
+                    item.style.transformOrigin = "50% 400%";
                     item.style.paddingTop = (-70 * (i + 1)) + "px";
                 }
             }
